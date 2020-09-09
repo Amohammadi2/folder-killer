6b78cfa37f63a9e818a5f27ab401d00326e202e6
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2020-09-09 21:40:41 +0430

    fixed: singular and plural form of

--- folder_killer.py
+++ folder_killer.py
@@ -30,7 +30,11 @@ def main():
 		print (e)
 		exit()
 	else:
-		print (f"{deleted_folders} empty folders deleted")
+		outputs = [
+			"{} empty folder deleted",
+			"{} empty folders deleted",
+		]
+		print (outputs[min(1, deleted_folders)+1].format(deleted_folders))
 
 
 if __name__ == "__main__":
