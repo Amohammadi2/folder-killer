396dce23fc04d2afa6b8bb2e36a2ab59b2ee7163
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2020-09-11 13:55:03 +0430

    logger.py file changed
    debug, warning and error function definitions are now replaced buy partial functions.
    Note: partial functions will be added to instance while initializing so they don't exist on the class it self
    __log function is now optimized and more error prune.
    singleton pattern is now impelmented by overwriting __new__ method on the class and so the `getInstance`
    method has been deleted

--- logger.py
+++ logger.py
@@ -3,22 +3,18 @@ class Logger:
 
     instance = None
 
-    def __init__(self, log_file="log.txt"):
-        if Logger.instance: raise Exception("this class is singleton, use `getInstance()` method instead")
-        Logger.instance = self
-        self.log_file = log_file
-     
-    def debug(self, message): self.__log(message, "debug")
-    def warning(self, message): self.__log(message, "warning")
-    def error(self, message): self.__log(message, "error")
-    
-    def __log(self, message, level):
-        log_message = "[{}] {}\n".format(level.upper(), message)
-        try:
-            with open(self.log_file, "a") as file:
-                file.write(log_message)
-        except FileNotFoundError:
-            open(self.log_file, "w").close() # create file
+    def __new__(cls):
+        if not cls.instance:
+            cls.instance = super().__new__(cls)
+        return cls.instance
 
-    @staticmethod
-    def getInstance(): return Logger.instance
+    def __init__(self, log_file="log.txt"):
+        self.log_file = log_file
+        self.debug = functools.partial(self.__log, "debug")
+        self.warning = functools.partial(self.__log, "warning")
+        self.error = functools.partial(self.__log, "error")
+    
+    def __log(self, level, message):
+        log_message = "[{}] {}\n".format(level.upper(), message)
+        with open(self.log_file, "a+") as file:
+            file.write(log_message)
